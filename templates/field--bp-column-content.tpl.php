200501757543880c59b8749fe9398c9f1d2916b6
Jim Birch <jim@xenomedia.com>
2017-10-24 12:57:20 -0500

    Updates Column field templates

--- templates/field--bp-column-content.tpl.php
+++ templates/field--bp-column-content.tpl.php
@@ -6,15 +6,9 @@
  */
 ?>
 
-<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php if (!$label_hidden): ?>
-    <div<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
-  <?php endif; ?>
-  <div<?php print $content_attributes; ?>>
-    <?php foreach ($items as $delta => $item): ?>
-      <div class="paragraph--type--bp-columns__<?php echo count($items); ?>col"<?php print $item_attributes[$delta]; ?>>
-        <?php print render($item); ?>
-      </div>
-    <?php endforeach; ?>
+<?php foreach ($items as $delta => $item): ?>
+  <div class="paragraph--type--bp-columns__<?php echo count($items); ?>col"<?php print $item_attributes[$delta]; ?>>
+    <?php print render($item); ?>
   </div>
-</div>
+<?php endforeach; ?>
+
